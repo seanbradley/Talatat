1e1ba0c6b753c211777236916afe618cf4f3af88
Sean Bradley <sean@blogblimp.com>
2015-02-19 04:05:36 -0800

    Refactoring as an API.

--- talatat/__init__.py
+++ talatat/__init__.py
@@ -1,12 +1,14 @@
 from pyramid.config import Configurator
-
-
+    
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    #config.include('pyramid_chameleon')
+    #config.add_static_view('static', 'static', cache_max_age=3600)
+    #config.add_route('index', '/')
+    config.add_route('musicians', '/musicians')
+    config.add_route('musician', '/musicians/{name}')
     config.scan()
     return config.make_wsgi_app()
+
--- talatat/views.py
+++ talatat/views.py
@@ -1,6 +1,104 @@
 from pyramid.view import view_config
 
+CITIES = {
+    'paris': {
+        'name': 'Paris',
+        'population': '2,234,105'
+    },
+    'sf': {
+        'name': 'San Francisco',
+        'population': '812,826'
+    }
+}
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'talatat'}
+MUSICIANS = {
+    "sean": {
+        "name":"Sean", 
+        "instrument":"Violin",
+        "instrument_maker":"Melanson", 
+        "conservatory":"Eastman",
+        "referral":"Heifetz",
+        "reviews": [{
+        "stars": 5,
+        "body": "I love this violinist!",
+        "author": "joe@example.com",
+        "createdOn": 1397490980837
+        }, {
+        "stars": 5,
+        "body": "Amazing musician!",
+        "author": "tim@example.com",
+        "createdOn": 1397490980837
+        }], 
+        "ontour":"false", 
+        "canbook":"true"
+    },
+    "rachel": {
+        "name":"Rachel", 
+        "instrument":"Electric Violin",
+        "instrument_maker":"Yamaha", 
+        "conservatory":"The Hague",
+        "referral":"Kremer",
+        "reviews": [{
+        "stars": 5,
+        "body": "So beautiful!",
+        "author": "sarah@example.com",
+        "createdOn": 1397490980837
+        }, {
+        "stars": 5,
+        "body": "Exquisite player!",
+        "author": "natasha@example.com",
+        "createdOn": 1397490980837
+        }], 
+        "ontour":"false", 
+        "canbook":"true"
+    },
+    "dima": {
+        "name":"Dima", 
+        "instrument":"Viola",
+        "instrument_maker":"Guarneri", 
+        "conservatory":"Moscow",
+        "referral":"Primrose",
+        "reviews": [{
+        "stars": 5,
+        "body": "Such a rich tone!",
+        "author": "sam@example.com",
+        "createdOn": 1397490980837
+        }, {
+        "stars": 3,
+        "body": "Jokes around on the job.",
+        "author": "eddie@example.com",
+        "createdOn": 1397490980837
+        }],  
+        "ontour":"false", 
+        "canbook":"true"
+    },
+    "judy": {
+        "name":"Judy", 
+        "instrument":"Cello",
+        "instrument_maker":"Stradivarius", 
+        "conservatory":"USC",
+        "referral":"Ma",
+        "reviews": [{
+        "stars": 5,
+        "body": "Never seen such a good-looking cellist!",
+        "author": "blaze@example.com",
+        "createdOn": 1397490980837
+        }, {
+        "stars": 2,
+        "body": "This cellist is such a flirt.",
+        "author": "susie@example.com",
+        "createdOn": 1397490980837
+        }],  
+        "ontour":"false", 
+        "canbook":"true"
+    }
+}
+
+@view_config(route_name='musician', renderer='json')
+def get_musician(request):
+    name = request.matchdict['name']
+    return MUSICIANS[name]
+
+@view_config(route_name='musicians', renderer='json')
+def list_musicians(request):
+    return MUSICIANS
