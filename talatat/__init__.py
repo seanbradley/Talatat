8dbdbb0a433369de558856a21da9e4a30e85dfbc
Sean Bradley <sean@blogblimp.com>
2015-02-21 02:50:34 -0800

    Incl. conn to Mongo, and refactor for URL traversal rather than dispatch.

--- talatat/__init__.py
+++ talatat/__init__.py
@@ -1,14 +1,48 @@
+from urlparse import urlparse 
+import json
+  
 from pyramid.config import Configurator
+from pyramid.renderers import JSON
+
+import pymongo
+from bson import json_util
+
+from talatat.models import Root
+
+
+class MongoJSONRenderer: 
+    def __init__(self, info): 
+        pass
+
+    def __call__(self, value, system): 
+        request = system.get('request') 
+        if request is not None: 
+            if not hasattr(request, 'response_content_type'): 
+                request.response_content_type = 'application/json' 
+        return json.dumps(value, default=json_util.default) 
     
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(settings=settings)
+    config = Configurator(settings=settings, root_factory=Root)
+    config.add_renderer('json', MongoJSONRenderer)
     #config.include('pyramid_chameleon')
     #config.add_static_view('static', 'static', cache_max_age=3600)
-    #config.add_route('index', '/')
+    
+    db_url = urlparse(settings['mongo_uri'])
+    config.registry.db = pymongo.Connection(
+       host=db_url.hostname,
+       port=db_url.port,
+    )
+
+    def add_db(request):
+       db = config.registry.db[db_url.path[1:]]
+       if db_url.username and db_url.password:
+           db.authenticate(db_url.username, db_url.password)
+       return db
+        
+    config.add_route('index', '/')
     config.add_route('musicians', '/musicians')
-    config.add_route('musician', '/musicians/{name}')
+    config.add_request_method(add_db, 'db', reify=True)
     config.scan()
     return config.make_wsgi_app()
-
--- talatat/views.py
+++ talatat/views.py
@@ -1,118 +1,20 @@
+import json
+
+from pyramid.view import forbidden_view_config, notfound_view_config
+from pyramid.response import Response
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPNotFound
 
-CITIES = {
-    'paris': {
-        'name': 'Paris',
-        'population': '2,234,105'
-    },
-    'sf': {
-        'name': 'San Francisco',
-        'population': '812,826'
-    }
-}
+from talatat.models import Root, Musician, Musicians
 
-MUSICIANS = {
-    "sean": {
-        "name":"Sean", 
-        "instrument":"Violin",
-        "instrument_maker":"Melanson", 
-        "conservatory":"Eastman",
-        "referral":"Heifetz",
-        "reviews": [{
-        "stars": 5,
-        "body": "I love this violinist!",
-        "author": "joe@example.com",
-        "createdOn": 1397490980837
-        }, {
-        "stars": 5,
-        "body": "Amazing musician!",
-        "author": "tim@example.com",
-        "createdOn": 1397490980837
-        }], 
-        "ontour":"false", 
-        "canbook":"true"
-    },
-    "rachel": {
-        "name":"Rachel", 
-        "instrument":"Electric Violin",
-        "instrument_maker":"Yamaha", 
-        "conservatory":"The Hague",
-        "referral":"Kremer",
-        "reviews": [{
-        "stars": 5,
-        "body": "So beautiful!",
-        "author": "sarah@example.com",
-        "createdOn": 1397490980837
-        }, {
-        "stars": 5,
-        "body": "Exquisite player!",
-        "author": "natasha@example.com",
-        "createdOn": 1397490980837
-        }], 
-        "ontour":"false", 
-        "canbook":"true"
-    },
-    "dima": {
-        "name":"Dima", 
-        "instrument":"Viola",
-        "instrument_maker":"Guarneri", 
-        "conservatory":"Moscow",
-        "referral":"Primrose",
-        "reviews": [{
-        "stars": 5,
-        "body": "Such a rich tone!",
-        "author": "sam@example.com",
-        "createdOn": 1397490980837
-        }, {
-        "stars": 3,
-        "body": "Jokes around on the job.",
-        "author": "eddie@example.com",
-        "createdOn": 1397490980837
-        }],  
-        "ontour":"false", 
-        "canbook":"true"
-    },
-    "judy": {
-        "name":"Judy", 
-        "instrument":"Cello",
-        "instrument_maker":"Stradivarius", 
-        "conservatory":"USC",
-        "referral":"Ma",
-        "reviews": [{
-        "stars": 5,
-        "body": "Never seen such a good-looking cellist!",
-        "author": "blaze@example.com",
-        "createdOn": 1397490980837
-        }, {
-        "stars": 2,
-        "body": "This cellist is such a flirt.",
-        "author": "susie@example.com",
-        "createdOn": 1397490980837
-        }],  
-        "ontour":"false", 
-        "canbook":"true"
-    }
-}
+import logging
+log = logging.getLogger(__name__)
 
-@view_config(route_name='musician', renderer='json')
-def get_musician(request):
-    name = request.matchdict['name']
-    return MUSICIANS[name]
+@view_config(renderer='json', context=Root)
+def index(context, request):
+    return {'info': 'Concert Talent API'}
 
-@view_config(route_name='musicians', renderer='json')
-def list_musicians(request):
-    return MUSICIANS
 
-#http method to UPDATE one record
-@view_config(request_method='PATCH', context=Musician, renderer='json')
-def update_musician(context, request):
-    r = context.update(request.json_body, True)
-
-    return Response(
-        status='202 Accepted',
-        content_type='application/json; charset=UTF-8')
-
-#http method to GET one record
 @view_config(request_method='GET', context=Musician, renderer='json')
 def get_musician(context, request):
     r = context.retrieve()
@@ -122,38 +24,49 @@ def get_musician(context, request):
     else:
         return r
 
-#http method to DELETE one record
+
 @view_config(request_method='DELETE', context=Musician, renderer='json')
 def delete_musician(context, request):
     context.delete()
 
     return Response(
         status='202 Accepted',
-        content_type='application/json; charset=UTF-8’)
+        content_type='application/json; charset=UTF-8')
 
-#http method to CREATE one record
-@view_config(request_method='PUT', context=Musicians, renderer='json')
+
+@view_config(request_method='POST', context=Musicians, renderer='json')
 def create_musician(context, request):
-    r = context.create(request.json_body)
+    context.create(request.json_body)
 
     return Response(
         status='201 Created',
         content_type='application/json; charset=UTF-8')
 
-#http method to GET multiple records
+
+@view_config(request_method='PUT', context=Musician, renderer='json')
+def update_musician(context, request):
+    context.update(request.json_body, True)
+
+    return Response(
+        status='202 Accepted',
+        content_type='application/json; charset=UTF-8')
+
+
 @view_config(request_method='GET', context=Musicians, renderer='json')
 def list_musicians(context, request):
     return context.retrieve()
 
-#error handling
+
 @notfound_view_config()
 def notfound(request):
     return Response(
-        body=json.dumps({'message': 'Not found!'}),
-        status='404 Not Found’,
-        content_type='application/json’)
+        body=json.dumps({'message': 'The requested musician or musicians cannot be found!'}),
+        status='404 Not Found',
+        content_type='application/json')
 
-#home
-@view_config(renderer='json', context=Root)
-def home(context, request):
-    return {'info': 'Talatat API'}
+@forbidden_view_config()
+def forbidden(request):
+    return Response(
+        body=json.dumps({'message': 'You are not authorized to access this data.'}),
+        status='401 Unauthorized',
+        content_type='application/json')
